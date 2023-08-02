3f1df6ff18d2755a8c2257a4db95cfb240236a34
Nathan Hoad <nathan@nathanhoad.net>
2023-08-02 23:40:29 +1000

    Fix issue with setting volume too early (#9)

--- addons/sound_manager/abstract_audio_player_pool.gd
+++ addons/sound_manager/abstract_audio_player_pool.gd
@@ -5,23 +5,31 @@ extends Node
 @export var default_pool_size := 8
 
 
-var available_players: Array = []
-var busy_players: Array = []
+var available_players: Array[AudioStreamPlayer] = []
+var busy_players: Array[AudioStreamPlayer] = []
 var bus: String = "Master"
 
 
-func _ready(possible_busses: Array = default_busses, pool_size: int = default_pool_size) -> void:
+func _init(possible_busses: PackedStringArray = default_busses, pool_size: int = default_pool_size) -> void:
 	for possible_bus in possible_busses:
-		if AudioServer.get_bus_index(possible_bus) > -1:
-			bus = possible_bus
-			break
-	
+		var cases: PackedStringArray = [
+			possible_bus,
+			possible_bus.to_lower(),
+			possible_bus.to_camel_case(),
+			possible_bus.to_pascal_case(),
+			possible_bus.to_snake_case()
+		]
+		for case in cases:
+			if AudioServer.get_bus_index(case) > -1:
+				bus = case
+				break
+
 	for i in pool_size:
 		increase_pool()
 
 
 func prepare(resource: AudioStream, override_bus: String = "") -> AudioStreamPlayer:
-	var player = get_available_player()
+	var player := get_available_player()
 	player.stream = resource
 	player.bus = override_bus if override_bus != "" else bus
 	player.volume_db = linear_to_db(1.0)
@@ -40,7 +48,7 @@ func get_available_player() -> AudioStreamPlayer:
 func mark_player_as_available(player: AudioStreamPlayer) -> void:
 	if busy_players.has(player):
 		busy_players.erase(player)
-		
+
 	if not available_players.has(player):
 		available_players.append(player)
 
@@ -51,7 +59,7 @@ func increase_pool() -> void:
 	available_players.append(player)
 	player.bus = bus
 	player.finished.connect(_on_player_finished.bind(player))
-  
+
 
 ### SIGNALS
 
