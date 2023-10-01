9ac46e8312eee35d21680f66711ba0fc7a56e813
Nathan Hoad <nathan@nathanhoad.net>
2023-10-01 22:38:07 +1000

    Tidy up C# wrapper

--- addons/sound_manager/SoundManager.cs
+++ addons/sound_manager/SoundManager.cs
@@ -3,146 +3,153 @@ using Godot.Collections;
 
 namespace NathanHoad
 {
-
   public partial class SoundManager : Node
   {
+    private static Node instance;
+    public static Node Instance
+    {
+      get
+      {
+        if (instance == null)
+        {
+          instance = (Node)Engine.GetSingleton("SoundManager");
+        }
+        return instance;
+      }
+    }
+
 
     #region Sounds
 
     public static float GetSoundVolume()
     {
-      return (float)GetSoundManager().Call("get_sound_volume");
+      return (float)Instance.Call("get_sound_volume");
     }
 
 
     public static float GetUISoundVolume()
     {
-      return (float)GetSoundManager().Call("get_ui_sound_volume");
+      return (float)Instance.Call("get_ui_sound_volume");
     }
 
 
     public static void SetSoundVolume(float volume)
     {
-      GetSoundManager().Call("set_sound_volume", volume);
+      Instance.Call("set_sound_volume", volume);
     }
 
 
     public static AudioStreamPlayer PlaySound(AudioStream resource, string overrideBus = "")
     {
-      return (AudioStreamPlayer)GetSoundManager().Call("play_sound", resource, overrideBus);
+      return (AudioStreamPlayer)Instance.Call("play_sound", resource, overrideBus);
     }
 
 
     public static AudioStreamPlayer PlayUISound(AudioStream resource, string overrideBus = "")
     {
-      return (AudioStreamPlayer)GetSoundManager().Call("play_ui_sound", resource, overrideBus);
+      return (AudioStreamPlayer)Instance.Call("play_ui_sound", resource, overrideBus);
     }
 
 
     public static void SetDefaultSoundBus(string bus)
     {
-      GetSoundManager().Call("set_default_sound_bus", bus);
+      Instance.Call("set_default_sound_bus", bus);
     }
 
 
     public static void SetDefaultUISoundBus(string bus)
     {
-      GetSoundManager().Call("set_default_ui_sound_bus", bus);
+      Instance.Call("set_default_ui_sound_bus", bus);
     }
 
     #endregion
+
 
     #region  Music
 
     public static float GetMusicVolume()
     {
-      return (float)GetSoundManager().Call("get_music_volume");
+      return (float)Instance.Call("get_music_volume");
     }
 
 
     public static void SetMusicVolume(float volume)
     {
-      GetSoundManager().Call("set_music_volume", volume);
+      Instance.Call("set_music_volume", volume);
     }
 
 
     public static AudioStreamPlayer PlayMusic(AudioStream resource, float crossFadeDuration = 0.0f, string overrideBus = "")
     {
-      return (AudioStreamPlayer)GetSoundManager().Call("play_music", resource, crossFadeDuration, overrideBus);
+      return (AudioStreamPlayer)Instance.Call("play_music", resource, crossFadeDuration, overrideBus);
     }
 
 
     public static AudioStreamPlayer PlayMusicAtVolume(AudioStream resource, float volume, float crossFadeDuration = 0.0f, string overrideBus = "")
     {
-      return (AudioStreamPlayer)GetSoundManager().Call("play_music_at_volume", resource, volume, crossFadeDuration, overrideBus);
+      return (AudioStreamPlayer)Instance.Call("play_music_at_volume", resource, volume, crossFadeDuration, overrideBus);
     }
 
 
     public static Array<string> GetMusicTrackHistory()
     {
-      return (Array<string>)GetSoundManager().Call("get_music_track_history");
+      return (Array<string>)Instance.Call("get_music_track_history");
     }
 
 
     public static string GetLastPlayedMusicTrack()
     {
-      return (string)GetSoundManager().Call("get_last_played_music_track");
+      return (string)Instance.Call("get_last_played_music_track");
     }
 
 
     public static bool IsMusicPlaying(AudioStream resource = null)
     {
-      return (bool)GetSoundManager().Call("is_music_playing", resource);
+      return (bool)Instance.Call("is_music_playing", resource);
     }
 
 
     public static bool IsMusicTrackPlaying(string resource_path)
     {
-      return (bool)GetSoundManager().Call("is_music_track_playing", resource_path);
+      return (bool)Instance.Call("is_music_track_playing", resource_path);
     }
 
 
     public static Array<AudioStream> GetCurrentlyPlayingMusic()
     {
-      return (Array<AudioStream>)GetSoundManager().Call("get_currently_playing_music");
+      return (Array<AudioStream>)Instance.Call("get_currently_playing_music");
     }
 
 
     public static Array<string> GetCurrentlyPlayingTracks()
     {
-      return (Array<string>)GetSoundManager().Call("get_currently_playing_tracks");
+      return (Array<string>)Instance.Call("get_currently_playing_tracks");
     }
 
 
     public static void PauseMusic(AudioStream resource = null)
     {
-      GetSoundManager().Call("pause_music", resource);
+      Instance.Call("pause_music", resource);
     }
 
 
     public static void ResumeMusic(AudioStream resource = null)
     {
-      GetSoundManager().Call("resume_music", resource);
+      Instance.Call("resume_music", resource);
     }
 
 
     public static void StopMusic(float fadeOutDuration = 0.0f)
     {
-      GetSoundManager().Call("stop_music", fadeOutDuration);
+      Instance.Call("stop_music", fadeOutDuration);
     }
 
 
     public static void SetDefaultMusicBus(string bus)
     {
-      GetSoundManager().Call("set_default_music_bus", bus);
+      Instance.Call("set_default_music_bus", bus);
     }
 
     #endregion
-
-
-    private static Node GetSoundManager()
-    {
-      return Engine.GetSingleton("SoundManager") as Node;
-    }
   }
 }
